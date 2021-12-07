924641379409d6035870f8896a8137462dfb6d21
Justin Clark <justin.clark@climate.com>
2021-12-07 01:14:09 -0600

    Show comparison between fast and slow way

--- Day7/secondPart.scala
+++ Day7/secondPart.scala
@@ -23,7 +23,24 @@ def binarySearchMovePrice(spawns: List[Long], low: Long, high: Long): Long = {
     math.min(midPointValue, binarySearchMovePrice(spawns, midPoint + 1, high))
 }
 
+def getMinimumMovePrice(spawns: List[Long]): Long = {
+  (0L until spawns(spawns.size - 1)).foldLeft(10000000000L)((acc, destination) =>
+      math.min(acc, getTotalMovePrice(spawns, destination)))
+}
+
 val filename = "input.txt"
-val spawns = Source.fromFile(filename).getLines.toList(0).split(",").toList.map(_.toLong).sorted
-println(spawns)
-println(binarySearchMovePrice(spawns, spawns(0), spawns(spawns.size - 1)))
+val spawns = Source.fromFile(filename).getLines().toList(0).split(",").toList.map(_.toLong).sorted
+
+val t1 = System.nanoTime
+val slowSolution = getMinimumMovePrice(spawns)
+val t2 = System.nanoTime
+val solution = binarySearchMovePrice(spawns, spawns(0), spawns(spawns.size - 1))
+val t3 = System.nanoTime
+val slowTime  = (t2 - t1)/1e9d
+val fastTime = (t3 - t2)/1e9d
+
+assert(slowSolution == solution)
+println("The solution is: " + solution)
+println("It took: " + slowTime + "s to find the solution the slow way")
+println("It took: " + fastTime + "s to find the solution the fast way")
+println("Sped up code is: " + slowTime / fastTime + " times faster")
