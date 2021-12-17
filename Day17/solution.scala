e40dce4b58f568f870b01cd5cea0f20334a13cad
Justin Clark <justin.clark@climate.com>
2021-12-17 00:12:55 -0600

    Make solution for functional

--- Day17/solution.scala
+++ Day17/solution.scala
@@ -13,7 +13,7 @@ def findNewVelocity(velocity: (Int, Int)): (Int, Int) =
   ((if (velocity._1 < 0) velocity._1 + 1 else if (velocity._1 > 0) velocity._1 - 1 else 0), velocity._2 - 1)
 
 def findElevationOfVelocity(xRange: (Int, Int), yRange: (Int, Int), velocity: (Int, Int)): Int = {
-  (for (i <- 1 to 300) yield i).foldLeft((0, (0, 0), velocity))((info, iter) => {
+  (1 to 300).foldLeft((0, (0, 0), velocity))((info, iter) => {
     val highest = info._1
     val pos = info._2
     val vel = info._3
@@ -30,13 +30,14 @@ def findElevationOfVelocity(xRange: (Int, Int), yRange: (Int, Int), velocity: (I
 }
 
 def findHighestElevationPoint(xRange: (Int, Int), yRange: (Int, Int)): (Int, Int) = {
-  var counter = 0
-  val highestTotalElev = (for (x <- 0 to xRange._2; y <- yRange._1 to 135) yield (x, y)).foldLeft(-1)((acc, vel) => {
-      val highestElev = findElevationOfVelocity(xRange, yRange, vel)
-      if (highestElev != -1)
-        counter += 1
-      math.max(acc,  highestElev)
-  })
+  val highestElevMappings = (0 to xRange._2).map(x => (yRange._1 to 135).map(y => {
+    val highestElev = findElevationOfVelocity(xRange, yRange, (x, y))
+    highestElev
+  }))
+
+  val counter = highestElevMappings.foldLeft(0)((acc, row) => acc + row.foldLeft(0)((rowAcc, value) => rowAcc + (if (value != -1) 1 else 0)))
+  val highestTotalElev = highestElevMappings.foldLeft(0)((acc, row) => math.max(acc, row.foldLeft(0)((rowAcc, value) => math.max(rowAcc, value))))
+
   (highestTotalElev, counter)
 }
 
